9b146313c8c822aecbf25458f241276731e5026a
Marciel032 <44622170+Marciel032@users.noreply.github.com>
2021-10-18 18:03:51 -0300

    Adicionado suporte ao atributo Display, para informar o nome da propriedade

--- Source/RelatorioVM/Extensoes/PropriedadeExtensao.cs
+++ Source/RelatorioVM/Extensoes/PropriedadeExtensao.cs
@@ -4,6 +4,7 @@ using RelatorioVM.Infraestruturas;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 using System.Text;
 
@@ -12,9 +13,13 @@ namespace RelatorioVM.Extensoes
     internal static class PropriedadeExtensao
     {
         public static string ObterNome(this PropertyInfo propriedade) {
-            var nome = propriedade.GetCustomAttribute<DisplayNameAttribute>();
-            if (nome != null && !string.IsNullOrWhiteSpace(nome.DisplayName))
-                return nome.DisplayName;
+            var atributoDisplayName = propriedade.GetCustomAttribute<DisplayNameAttribute>();
+            if (atributoDisplayName != null && !string.IsNullOrWhiteSpace(atributoDisplayName.DisplayName))
+                return atributoDisplayName.DisplayName;
+
+            var atributoDisplay = propriedade.GetCustomAttribute<DisplayAttribute>();
+            if (atributoDisplay != null && !string.IsNullOrWhiteSpace(atributoDisplay.Name))
+                return atributoDisplay.Name;
 
             return propriedade.Name;
         }
